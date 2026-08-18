6eca83b87fa99741530a9208149740be4b6c7c9d
Danilo Macri <danilomacri11@gmail.com>
2026-08-18 20:37:57 -0300

    Habilitando crude no views.py

--- Aula03/produtos/views.py
+++ Aula03/produtos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Produto
+from .serializers import ProdutoSerializer
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Olá Django ! Aplicacoes Web 2026 - 2 - Aula 03 Loja de Produtos")
+
+class ProdutoViewSet(viewsets.ModelViewSet):
+    queryset = Produto.objetcs.all().order_by("-id")
+    serializer_class = ProdutoSerializer
