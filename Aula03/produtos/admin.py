df79fa48baf46e25564956ffce0829a87fce12f8
Danilo Macri <danilomacri11@gmail.com>
2026-08-18 20:13:12 -0300

    admin e apps basico

--- Aula03/produtos/admin.py
+++ Aula03/produtos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Produto
 
 # Register your models here.
+@admin.register(Produto)
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome", "quantidade", "preco", "created_at")
+    search_fields = ("nome")
