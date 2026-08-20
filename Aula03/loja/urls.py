88e55e7f27a177aedd0ae3c41828dc7b8282bca3
Danilo Macri <danilomacri11@gmail.com>
2026-08-20 19:53:53 -0300

    Classe categoria

--- Aula03/loja/urls.py
+++ Aula03/loja/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+from rest_framework.routers import DefaultRouter
+from produtos.views import ProdutoViewSet
+
+def home(request):
+    return HttpResponse("Olá Django ! Aplicacoes Web 2026 - 2 - Aula 03 Loja de Produtos")
+
+router = DefaultRouter()
+router.register(r'produtos', ProdutoViewSet, basename = 'produto')
 
 urlpatterns = [
+    path('',home),
     path('admin/', admin.site.urls),
+    path('api/', include{router.urls})
 ]
